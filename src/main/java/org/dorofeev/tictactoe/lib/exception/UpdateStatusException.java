ec617b46ca199f155e0e514629cdd61f1a6bb937
yury <u.dorofeev@gmail.com>
2016-08-02 17:32:40 +0200

    small changes

---
+++ src/main/java/org/dorofeev/tictactoe/lib/exception/UpdateStatusException.java
@@ -0,0 +1,12 @@
+package org.dorofeev.tictactoe.lib.exception;
+
+/**
+ * UpdateStatusException
+ * @author Yury Dorofeev
+ * @version 2015-12-04
+ */
+public class UpdateStatusException extends Exception {
+    public UpdateStatusException(String message){
+        super(message);
+    }
+}
