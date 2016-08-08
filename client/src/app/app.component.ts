f3b108e48ddf7c10b3c56a147f37b6e25440734e
yury <u.dorofeev@gmail.com>
2016-08-08 16:19:25 +0200

    Experiments with Angular 2.0 are in progress ....

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-const HEROES: Hero[] = [
-{ id: 11, name: 'Mr. Nice' },
-{ id: 12, name: 'Narco' },
-{ id: 13, name: 'Bombasto' },
-{ id: 14, name: 'Celeritas' },
-{ id: 15, name: 'Magneta' },
-{ id: 16, name: 'RubberMan' },
-{ id: 17, name: 'Dynama' },
-{ id: 18, name: 'Dr IQ' },
-{ id: 19, name: 'Magma' },
-{ id: 20, name: 'Tornado' }
-];
+import { HeroService } './hero.service';
+import { OnInit } '@angular/core';
 
 @Component({
     selector: 'my-app',
@@ -77,13 +66,23 @@ const HEROES: Hero[] = [
             border-radius: 4px 0 0 4px;
           }
     `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        this.getHeroes();
+    }
+
     title = 'Tour of Heroes';
     selectedHero: Hero;
-    heroes = HEROES;
+    heroes: Hero[];
+
+    constructor(private heroService: HeroService) {}
 
     onSelect(hero: Hero) { this.selectedHero = hero; }
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 }
---
+++ client/src/app/hero.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+}
