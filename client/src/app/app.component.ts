a6408b25048aa82bed4699dc875119508098f088
yury <u.dorofeev@gmail.com>
2016-08-08 11:20:59 +0200

    The first frontend context is fixed

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Tic Tac Toe</h1>'
+})
+export class AppComponent { }
--- client/src/systemjs.config.js
+++ client/src/systemjs.config.js
@@ -1,4 +1,4 @@
-**
+/**
  * System configuration for Angular 2 samples
  * Adjust as necessary for your application needs.
  */
